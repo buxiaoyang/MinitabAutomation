b7201eb41e90fafecc44f4755a546151573d6ff3
buxiaoyang <buxiaoyang@gmail.com>
2015-03-18 17:41:55 +0800

    init commit

---
+++ MinitabAutomation/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace MinitabAutomation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxOutPut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(575, 51);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(478, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(97, 51);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(478, 51);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(67, 51);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel5.Location = new System.Drawing.Point(403, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(75, 51);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.textBoxFile);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(67, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(0, 17, 0, 0);
+            this.panel6.Size = new System.Drawing.Size(336, 51);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label2);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel7.Location = new System.Drawing.Point(0, 390);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(575, 29);
+            this.panel7.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.textBoxOutPut);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel8.Location = new System.Drawing.Point(0, 51);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(575, 339);
+            this.panel8.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件：";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxFile.Location = new System.Drawing.Point(0, 17);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(336, 22);
+            this.textBoxFile.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(5, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 41);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "开始处理";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxOutPut
+            // 
+            this.textBoxOutPut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOutPut.Location = new System.Drawing.Point(0, 0);
+            this.textBoxOutPut.Multiline = true;
+            this.textBoxOutPut.Name = "textBoxOutPut";
+            this.textBoxOutPut.Size = new System.Drawing.Size(575, 339);
+            this.textBoxOutPut.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2015年 版本1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 419);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minitab自动化 V1.0";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxOutPut;
+    }
+}
+
---
+++ MinitabAutomation/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MinitabAutomation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("Mtb"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            Mtb.Application MtbApp = new Mtb.Application();
+            MtbApp.UserInterface.Visible = true;
+            Console.WriteLine("Status = " + MtbApp.Status);
+            Console.WriteLine("LastError = " + MtbApp.LastError);
+            Console.WriteLine("Application Path = " + MtbApp.AppPath);
+            Console.WriteLine("Window Handle = " + MtbApp.Handle);
+
+            Mtb.Project MtbProj = MtbApp.ActiveProject;
+            /*
+            Mtb.Columns MtbColumns = MtbProj.ActiveWorksheet.Columns;
+            Mtb.Column MtbColumn1 = MtbColumns.Add(null,null,1);
+            MtbColumn1.Name = "缺陷项";
+            String[] data1 = {"虚焊","漏焊","强度不够","外观受损","其他"};
+            MtbColumn1.SetData(data1);
+
+            Mtb.Column MtbColumn2 = MtbColumns.Add(null, null, 1);
+            MtbColumn2.Name = "数量";
+            Double[] data2 = { 500, 300, 200, 150, 160};
+            MtbColumn2.SetData(data2);
+            */
+
+            Mtb.Columns MtbColumns = MtbProj.ActiveWorksheet.Columns;
+            Mtb.Column MtbColumn1 = MtbColumns.Add(null, null, 1);
+            int[] data1 = { 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6193, 6192, 6193, 6193, 6192, 6193, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6193, 6192, 6193, 6193, 6193, 6192, 6193, 6193, 6193, 6193, 6192, 6193, 6193, 6192, 6193, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192, 6192 };
+            MtbColumn1.SetData(data1);
+
+            Mtb.Column MtbColumn2 = MtbColumns.Add(null, null, 1);
+            DateTime[] data2 = { DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-08"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07"), DateTime.Parse("2013-10-07") };
+            MtbColumn2.SetData(data2);
+
+            Mtb.Column MtbColumn3 = MtbColumns.Add(null, null, 1);
+            Double[] data3 = { 1808, 1796.3, 1799, 1800.8, 1800.5, 1803.6, 1794.5, 1796.9, 1802.1, 1796.5, 1795.5, 1812.8, 1795.8, 1805.6, 1798.6, 1799.6, 1803.7, 1800.9, 1799.4, 1808.6, 1800.3, 1804, 1804.4, 1798.8, 1802.1, 1810.1, 1797, 1809.4, 1793.3, 1803.6, 1801, 1797.5, 1797.8, 1796.4, 1796.1, 1793.9, 1809.7, 1795.3, 1798.9, 1796.6, 1793.6, 1794.4, 1798.5, 1806.2, 1807, 1805.4, 1806.9, 1800.7, 1799.5, 1802.3, 1791.8, 1795.5, 1798.5, 1801.1, 1794.2, 1799.4, 1792.5, 1803.7, 1805.1, 1808.9, 1795.6, 1806.3, 1799.5, 1798.9, 1799.6, 1795.8, 1799.9, 1795.1, 1806.4, 1809.6, 1808, 1796.1, 1797.1, 1793.1, 1802.6, 1800, 1796.5, 1799.3, 1798.6, 1808, 1803.2, 1810.2, 1793.9, 1798.6, 1808, 1797, 1793.2, 1801.6, 1803.4, 1801.5, 1794.5, 1801.2, 1807.3, 1796.3, 1801.7, 1797.5, 1794.5, 1797.7, 1798.5, 1794.2, 1797.2, 1800.4, 1794.9, 1797.5, 1797.1, 1796.3, 1796.8, 1805.7, 1798.7, 1796.7, 1799, 1793.3, 1798.4, 1809.6, 1793.2, 1791.8, 1796.8, 1797.1, 1800.7, 1797.3, 1799.5, 1807, 1802.7, 1800.9, 1790, 1805.9, 1802.6, 1800.8, 1795.9, 1801.9, 1807.8, 1798.4, 1795.6, 1789.7, 1801.9, 1797.9, 1799.5, 1805.8, 1804.8, 1795.7, 1797.4, 1801.8, 1788.5, 1802.3, 1800.3, 1798.3, 1802.6, 1794.8, 1799, 1794.5, 1800.2, 1799.8, 1798, 1797.7, 1804.7, 1800.9, 1799.5, 1797, 1796.4, 1801.5, 1801.4, 1801.8, 1813.7, 1796.4, 1805.4, 1797.6, 1805, 1792.6, 1809.8, 1799.8, 1804.8, 1798.9, 1801.4, 1798.1, 1802.4, 1803.7, 1796.9, 1808.8, 1798.9, 1795.8, 1803.8, 1796.2, 1797.2, 1797.5, 1803.2, 1803, 1805.4, 1796.7, 1795.1, 1796, 1794.5, 1801.7, 1803.5, 1806.5, 1799, 1793.6, 1812.2, 1809.6, 1801.2, 1802.2, 1802.4, 1796.5, 1812.8, 1795.2, 1793.1, 1804, 1799.9, 1797, 1799.3, 1801.6, 1798.1, 1794.8, 1801.5, 1810, 1799.2, 1798.5, 1795.7, 1792.9, 1801.8, 1803.4, 1798.9, 1801.5, 1804, 1802.3, 1797.1, 1795.7, 1797.8, 1801.3, 1796.6, 1800.4, 1798, 1803.4, 1805.7, 1800.6, 1801.4, 1801.8, 1798.7, 1805.9, 1794.8, 1800.6, 1795.7, 1811.2, 1798.3, 1792.4, 1801.8, 1805.6, 1806.9, 1798.4, 1797.9, 1795.4, 1801.2, 1800, 1801, 1793, 1797.4, 1805.6, 1793.9, 1794.9, 1797, 1795.3, 1801.7, 1799.7, 1805.1, 1799.5, 1796.9, 1797.9, 1795.7, 1794.4, 1796.4, 1797.1, 1798.4, 1804.3, 1800.2, 1799.1, 1792, 1811.3, 1802, 1798.8, 1804.9, 1792.8, 1792.4, 1791.4, 1794.7, 1796.6, 1793.8, 1799.2, 1794.7, 1799.9, 1790.1, 1795.1, 1799.6, 1795.7, 1796.5, 1800.1, 1801.6, 1797.2, 1790.5, 1799, 1794.3, 1803.5, 1794.1, 1794.5, 1800, 1801.8 };
+            MtbColumn3.SetData(data3);
+
+            MtbProj.ExecuteCommand(" Capa C3 304;   Lspec 1764;   Uspec 1836;   Pooled;   AMR;   UnBiased;   OBiased;   Toler 6;   Within;   Percent;   Title \"aaaaa\";   CStat.");
+            Mtb.Graph MtbGraph = MtbProj.Commands.Item(1).Outputs.Item(1).Graph;
+            MtbGraph.SaveAs("C:\\MyGraph" + DateTime.Now.ToString("yyyy-MM-dd HHmmssffff"), true, Mtb.MtbGraphFileTypes.GFPNGHighColor, 600, 400);
+
+            MtbProj.ExecuteCommand("  Indplot ( C3 ) * C1;   Title \"bbbbb\";   Individual.");
+            Mtb.Graph MtbGraph2 = MtbProj.Commands.Item(2).Outputs.Item(1).Graph;
+            MtbGraph2.SaveAs("C:\\MyGraph" + DateTime.Now.ToString("yyyy-MM-dd HHmmssffff"), true, Mtb.MtbGraphFileTypes.GFPNGHighColor, 600, 400);
+
+            MtbProj.ExecuteCommand("  Plot C3*C2;   Symbol C1;   Title \"ccccc\";   JITTER.");
+            Mtb.Graph MtbGraph3 = MtbProj.Commands.Item(3).Outputs.Item(1).Graph;
+            MtbGraph3.SaveAs("C:\\MyGraph" + DateTime.Now.ToString("yyyy-MM-dd HHmmssffff"), true, Mtb.MtbGraphFileTypes.GFPNGHighColor, 600, 400);
+
+            MtbProj.ExecuteCommand(" PPlot C3;   Normal;   Symbol;   FitD;     NoCI;   Grid 2;   Grid 1;   MGrid 1;   Title \"dddddd\".");
+            Mtb.Graph MtbGraph4 = MtbProj.Commands.Item(4).Outputs.Item(1).Graph;
+            MtbGraph4.SaveAs("C:\\MyGraph" + DateTime.Now.ToString("yyyy-MM-dd HHmmssffff"), true, Mtb.MtbGraphFileTypes.GFPNGHighColor, 600, 400);
+
+            MtbApp.Quit();
+        }
+    }
+}
