723e3ee6f4efe86b327027e6a6578ed4b100d224
buxiaoyang <buxiaoyang@gmail.com>
2015-05-27 15:04:40 +0800

    screen shot added.

--- MinitabAutomation/Form1.Designer.cs
+++ MinitabAutomation/Form1.Designer.cs
@@ -59,7 +59,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(575, 51);
+            this.panel1.Size = new System.Drawing.Size(575, 48);
             this.panel1.TabIndex = 0;
             // 
             // panel3
@@ -70,7 +70,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(478, 51);
+            this.panel3.Size = new System.Drawing.Size(478, 48);
             this.panel3.TabIndex = 1;
             // 
             // panel6
@@ -79,16 +79,16 @@
             this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel6.Location = new System.Drawing.Point(67, 0);
             this.panel6.Name = "panel6";
-            this.panel6.Padding = new System.Windows.Forms.Padding(0, 17, 0, 0);
-            this.panel6.Size = new System.Drawing.Size(336, 51);
+            this.panel6.Padding = new System.Windows.Forms.Padding(0, 16, 0, 0);
+            this.panel6.Size = new System.Drawing.Size(336, 48);
             this.panel6.TabIndex = 2;
             // 
             // textBoxFile
             // 
             this.textBoxFile.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxFile.Location = new System.Drawing.Point(0, 17);
+            this.textBoxFile.Location = new System.Drawing.Point(0, 16);
             this.textBoxFile.Name = "textBoxFile";
-            this.textBoxFile.Size = new System.Drawing.Size(336, 22);
+            this.textBoxFile.Size = new System.Drawing.Size(336, 25);
             this.textBoxFile.TabIndex = 0;
             // 
             // panel5
@@ -97,14 +97,14 @@
             this.panel5.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel5.Location = new System.Drawing.Point(403, 0);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(75, 51);
+            this.panel5.Size = new System.Drawing.Size(75, 48);
             this.panel5.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 12);
+            this.button1.Location = new System.Drawing.Point(3, 14);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 29);
+            this.button1.Size = new System.Drawing.Size(69, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "Select";
             this.button1.UseVisualStyleBackColor = true;
@@ -116,15 +116,15 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(67, 51);
+            this.panel4.Size = new System.Drawing.Size(67, 48);
             this.panel4.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.Size = new System.Drawing.Size(54, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "File：";
             // 
@@ -134,8 +134,8 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel2.Location = new System.Drawing.Point(478, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.panel2.Size = new System.Drawing.Size(97, 51);
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(97, 48);
             this.panel2.TabIndex = 0;
             // 
             // button2
@@ -144,7 +144,7 @@
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button2.Location = new System.Drawing.Point(5, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 41);
+            this.button2.Size = new System.Drawing.Size(87, 38);
             this.button2.TabIndex = 0;
             this.button2.Text = "Process";
             this.button2.UseVisualStyleBackColor = false;
@@ -154,9 +154,9 @@
             // 
             this.panel7.Controls.Add(this.label2);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel7.Location = new System.Drawing.Point(0, 390);
+            this.panel7.Location = new System.Drawing.Point(0, 366);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(575, 29);
+            this.panel7.Size = new System.Drawing.Size(575, 27);
             this.panel7.TabIndex = 1;
             // 
             // label2
@@ -164,7 +164,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 5);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.Size = new System.Drawing.Size(135, 15);
             this.label2.TabIndex = 0;
             this.label2.Text = "2015 Version 1.0";
             // 
@@ -172,9 +172,9 @@
             // 
             this.panel8.Controls.Add(this.textBoxOutPut);
             this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel8.Location = new System.Drawing.Point(0, 51);
+            this.panel8.Location = new System.Drawing.Point(0, 48);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(575, 339);
+            this.panel8.Size = new System.Drawing.Size(575, 318);
             this.panel8.TabIndex = 2;
             // 
             // textBoxOutPut
@@ -184,14 +184,14 @@
             this.textBoxOutPut.Multiline = true;
             this.textBoxOutPut.Name = "textBoxOutPut";
             this.textBoxOutPut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxOutPut.Size = new System.Drawing.Size(575, 339);
+            this.textBoxOutPut.Size = new System.Drawing.Size(575, 318);
             this.textBoxOutPut.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 419);
+            this.ClientSize = new System.Drawing.Size(575, 393);
             this.Controls.Add(this.panel8);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel1);
